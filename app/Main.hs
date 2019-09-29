434837ace0976ae963bd605ac81a8b9b10e3f007
Yan Shkurinsky <yan.shkurinsky@gmail.com>
2019-09-29 22:52:23 +0300

    Path handling and trying read config

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,38 @@
 
 module Main where
 
-import System.Environment ( getArgs )
-import System.Exit ( exitFailure )
-import Control.Monad ( when )
-import qualified Logger as Logger
-import qualified Data.Bytestring
-import qualified Logger.Impl.Console as ConsoleLogger
+import MyPrelude
+import Control.Exception              ( SomeException, try )
+import System.Environment             ( getArgs )
+import System.Exit                    ( exitFailure )
+import Control.Monad                  ( when )
+import Data.Aeson                     ( decodeStrict )
+import qualified Logger               as Logger
+import qualified Logger.Impl.Console  as ConsoleLogger
+
+
+type ReadFileExpection a = IO ( Either SomeException a )
 
 main :: IO ()
 main = 
   ConsoleLogger.withHandle $ \ logger -> do
+  let logFatal = Logger.logFatal logger
+  -- |Passing config path in command-ilne. 
+  -- Get args and check its count
   arguments <- getArgs
   when (length arguments /= 1) $ do
-    Logger.logFatal logger "Invalid count of arguments" >> exitFailure
-  let [configPath] = arguments  
+    logFatal "Invalid count of arguments" >> exitFailure
+  let [configPath] = arguments 
   
+  -- |Try to read config
+  readConfigResult <- try $
+    b8ReadFile configPath :: ReadFileExpection ByteStringChar8
+  when (isLeft readConfigResult) $ do
+    logFatal $
+      "Can't read configuration from path"
+      <> tPack configPath <> "/n"
+    exitFailure
+  let (Right b8Config) = readConfigResult
+
   undefined
+
--- src/MyPrelude.hs
+++ src/MyPrelude.hs
@@ -1,3 +1,32 @@
-module MyPrelude where
+module MyPrelude 
+  (
+  -- |ByteString.Char8 exports
+    b8Pack, b8Unpack, b8ReadFile
+  , ByteStringChar8
 
+  -- |Text exports
+  , tPack, tUnpack, Text
 
+  -- |Either exports
+  , isLeft, isRight
+
+  )where
+
+import qualified Data.ByteString.Char8  as B
+import qualified Data.Text              as T
+import Data.Either                      ( isLeft, isRight )
+
+-- |ByteString.Char8 functoins 
+b8ReadFile  = B.readFile
+b8Pack      = B.pack
+b8Unpack    = B.unpack
+
+-- |ByteString.Char8 types
+type ByteStringChar8  = B.ByteString
+
+-- |Text functoins
+tPack   = T.pack
+tUnpack = T.unpack
+
+-- |Text types
+type Text = T.Text
