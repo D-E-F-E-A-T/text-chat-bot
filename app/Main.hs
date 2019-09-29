59e6ca433f66e35e36ad8063e14aebd53b373e59
Yan Shkurinsky <yan.shkurinsky@gmail.com>
2019-09-29 20:32:56 +0300

    Adding of custom prelude

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import System.Environment ( getArgs )
+import System.Exit ( exitFailure )
+import Control.Monad ( when )
+import qualified Logger as Logger
+import qualified Data.Bytestring
+import qualified Logger.Impl.Console as ConsoleLogger
 
 main :: IO ()
-main = undefined
+main = 
+  ConsoleLogger.withHandle $ \ logger -> do
+  arguments <- getArgs
+  when (length arguments /= 1) $ do
+    Logger.logFatal logger "Invalid count of arguments" >> exitFailure
+  let [configPath] = arguments  
+  
+  undefined
