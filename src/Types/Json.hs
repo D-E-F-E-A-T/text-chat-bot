91f09730a1a6ddeb9ccce1f663d4d809c1c2a65a
Yan Shkurinsky <yan.shkurinsky@gmail.com>
2019-09-30 08:34:12 +0300

    Adding of JSON implementation for config data-type

--- src/Types/Json.hs
+++ src/Types/Json.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Types.Json where
 
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Text ( Text )
 
--- |Main configuration data-type
+-- |Main configuration file data-types
 data Config = Config 
   { messengerConfig :: [MessengerConfig] }
 
@@ -16,3 +17,14 @@ data MessengerConfig = MessengerConfig
 data Proxy = Proxy
   { host :: Text
   , port :: Int }
+
+instance FromJSON Proxy where
+  parseJSON (Object proxy) = Proxy <$> proxy .: "host" <*> proxy .: "port"
+
+instance FromJSON MessengerConfig where
+  parseJSON (Object m) = MessengerConfig  <$> m .: "name"
+                                          <*> m .: "token"
+                                          <*> m .:? "proxy"
+
+instance FromJSON Config where
+  parseJSON (Object c) = Config <$> c .: "messengers"
